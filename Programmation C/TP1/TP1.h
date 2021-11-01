dfe7f109b3a37db4e9dbcf617211f1561fd1b261
Zyad Benkirane <84535224+Zeo-22@users.noreply.github.com>
2021-11-01 18:23:22 +0100

    Delete Programmation C directory

--- Programmation C/TP1/TP1.h
+++
@@ -1,6 +0,0 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include "TP1.c"
-void check_msb(int num);
-void print_bits(unsigned char byte);
-
--- Programmation C/TP1/mainTP1.c
+++
@@ -1,8 +0,0 @@
-#include "TP1.h"
-
-int main(){
-	check_msb(4);
-	printf("\nThe character's binary code is : ");
-	print_bits(5);
-	return 0;
-}
